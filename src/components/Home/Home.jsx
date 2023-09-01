461919acd6a79ed78b6d016fa976ed7d0faa184d
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-09-01 17:29:30 +0600

    understood the exit and enter animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,10 @@
 import { Helmet } from 'react-helmet-async';
 import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 
 export default function Home() {
+	const [showDiv, setShowDiv] = useState(true);
+
 	const containerVarian = {
 		initial: {
 			opacity: 0,
@@ -10,6 +13,10 @@ export default function Home() {
 		final: {
 			opacity: 1,
 			y: 0,
+		},
+		exit: {
+			opacity: 0,
+			y: 50,
 		},
 		hover: {
 			scale: 1.25,
@@ -52,39 +59,56 @@ export default function Home() {
 			</Helmet>
 			{/* Number one, Sakib Khan */}
 			<div className='h-56 flex justify-center items-center'>
-				<motion.div
-					className='h-28 aspect-square bg-blue-300'
-					initial={{ scale: 0 }}
-					animate={{ scale: 1 }}
-					whileHover={{ scale: 1.2 }}
-					transition={{
-						ease: 'anticipate',
-						duration: 3,
-						delay: 3,
-					}}></motion.div>
+				<AnimatePresence>
+					{showDiv && (
+						<motion.div
+							className='h-28 aspect-square bg-blue-300'
+							initial={{ scale: 0 }}
+							animate={{ scale: 1 }}
+							exit={{ scale: 0.5, transition: { delay: 0 } }}
+							whileHover={{ scale: 1.2 }}
+							onHoverStart={() => {
+								console.log('started');
+								setShowDiv(!showDiv);
+							}}
+							onHoverEnd={() => {
+								console.log('ended');
+								setShowDiv(!showDiv);
+								console.log(showDiv);
+							}}
+							transition={{
+								type: 'spring',
+								duration: 2,
+								bounce: 0.6,
+								// delay: 3,
+							}}></motion.div>
+					)}
+				</AnimatePresence>
 			</div>
 
 			{/* Number 2 */}
-			<div className='h-56 flex justify-center items-center'>
-				{/* <AnimatePresence> */}
-				<motion.div
-					className='bg-teal-300 p-10'
-					variants={containerVarian}
-					initial='initial'
-					animate='final'
-					whileHover='hover'
-					transition={{
-						type: 'tween',
-						duration: 5,
-						delayChildren: 1,
-						staggerChildren: 1,
-					}}>
-					<motion.li variants={childVariant}>child-1</motion.li>
-					<motion.li variants={childVariant}>child-2</motion.li>
-					<motion.li variants={childVariant}>child-3</motion.li>
-				</motion.div>
-				{/* </AnimatePresence> */}
-			</div>
+			{/* <div className='h-56 flex justify-center items-center'>
+				<AnimatePresence>
+					<motion.div
+						className='bg-teal-300 p-10'
+						variants={containerVarian}
+						initial='initial'
+						animate='final'
+						exit='exit'
+						whileHover='hover'
+						transition={{
+							type: 'tween',
+							duration: 1,
+							delayChildren: 1,
+							staggerChildren: 1,
+							yoyo: 5,
+						}}>
+						<motion.li variants={childVariant}>child-1</motion.li>
+						<motion.li variants={childVariant}>child-2</motion.li>
+						<motion.li variants={childVariant}>child-3</motion.li>
+					</motion.div>
+				</AnimatePresence>
+			</div> */}
 
 			{/* Number 3 */}
 			{/* <div className='h-56 flex justify-center items-center'>
@@ -103,7 +127,7 @@ export default function Home() {
 				</motion.div>
 			</div> */}
 
-			<motion.button
+			{/* <motion.button
 				className='text-white'
 				onTap={(e, i) => console.log(e, i, 'clicked')}
 				onTapStart={(e, i) => console.log(e, i, 'clicking')}
@@ -111,10 +135,12 @@ export default function Home() {
 					console.log(`x: ${i.velocity.x} and y: ${i.velocity.y}`)
 				}
 				onPanStart={(e, i) =>
-					console.log(`x: ${i.velocity.x} and y: ${i.velocity.y} started`)
+					console.log(
+						`x: ${i.velocity.x} and y: ${i.velocity.y} started`
+					)
 				}>
 				Tap
-			</motion.button>
+			</motion.button> */}
 		</div>
 	);
 }
